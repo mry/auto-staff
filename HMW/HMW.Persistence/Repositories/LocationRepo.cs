33d8caa133c8c0311018387eda3e8ec36a63b476
Johan Nicklasson <nicklasson_johan@hotmail.com>
2020-04-07 23:20:26 +0200

    location repository

---
+++ HMW/HMW.Persistence/Repositories/LocationRepo.cs
@@ -0,0 +1,14 @@
+﻿using HMW.Core.Interfaces;
+using HMW.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HMW.Persistence.Repositories
+{
+    public class LocationRepo : MongoCollectionBase<Location>, ILocationRepo
+    {
+        private const string COLLECTION_NAME = "Locations";
+        public LocationRepo(IDbConfig dbConfig) : base(dbConfig, COLLECTION_NAME) { }
+    }
+}
