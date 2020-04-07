1c18216efaf516874f8560a47c722d37041987c6
Johan Nicklasson <nicklasson_johan@hotmail.com>
2020-04-07 22:53:09 +0200

    OrganizationAbsenceHandler

---
+++ HMW/HMW.Core/Handlers/Absence/OrganizationAbsenceHandler.cs
@@ -0,0 +1,43 @@
+﻿using HMW.Core.Interfaces;
+using HMW.Core.Notifications.Absence;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HMW.Core.Handlers.Absence
+{
+    class OrganizationAbsenceHandler : INotificationHandler<CreateAbsenceNotification>
+    {
+        private readonly IOrganizationRepo organizationRepo;
+        private readonly IEmployeeRepo employeeRepo;
+
+        public OrganizationAbsenceHandler(IOrganizationRepo organizationRepo, IEmployeeRepo employeeRepo)
+        {
+            this.organizationRepo = organizationRepo;
+            this.employeeRepo = employeeRepo;
+        }
+
+        public Task Handle(CreateAbsenceNotification notification, CancellationToken cancellationToken)
+        {
+            var employee = employeeRepo.Get(notification.EmployeeId);
+            if (employee == null)
+            {
+                throw new Exception("No employee found");
+            }
+
+            var org = organizationRepo.Get(employee.OrganizationId);
+            if (org == null)
+            {
+                throw new Exception("No organization found");
+            }
+
+            // TODO:
+            // get HR-endpoint for this organization and send absence notification
+
+            return Task.FromResult(0);
+        }
+    }
+}
